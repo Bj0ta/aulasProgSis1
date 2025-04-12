8fc4101236c9480480bbce6f0730d966a1900297
Bianca Jesus <billdias123@gmail.com>
2025-04-12 10:37:59 -0300

    Add files via upload

---
+++ teste1/src/Pessoa.java
@@ -0,0 +1,24 @@
+public class Pessoa {
+    String nome;
+    int idade;
+    float altura;
+
+public Pessoa(String nome, int idade, float altura){
+    this.nome = nome;
+    this.idade = idade;
+    this.altura = altura;
+}
+public Pessoa(String nome, int idade){
+    this.nome = nome;
+    this.idade = idade;
+}
+public static void main(String[]args){
+    Pessoa p1 = new Pessoa("Luciana Roberto",30,1.70f);
+    Pessoa p2 = new Pessoa("Fernanda Fernandes",28,1.69f);
+}
+public void exibirInformacao(){
+    System.out.println("Nome: " + this.nome);
+    System.out.println("Idade: " + this.idade);
+    System.out.println("Altura: " + this.altura);
+}
+}
