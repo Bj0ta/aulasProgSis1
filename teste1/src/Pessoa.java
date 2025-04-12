28db416a67481e3507519aef5555d45d84165c0a
Bianca Jesus <billdias123@gmail.com>
2025-04-12 10:46:27 -0300

    Update Pessoa.java

--- teste1/src/Pessoa.java
+++ teste1/src/Pessoa.java
@@ -15,10 +15,12 @@ public Pessoa(String nome, int idade){
 public static void main(String[]args){
     Pessoa p1 = new Pessoa("Luciana Roberto",30,1.70f);
     Pessoa p2 = new Pessoa("Fernanda Fernandes",28,1.69f);
+    p1.exibirInformacao();
+    p2.exibirInformacao();
 }
 public void exibirInformacao(){
     System.out.println("Nome: " + this.nome);
     System.out.println("Idade: " + this.idade);
     System.out.println("Altura: " + this.altura);
-}
+} 
 }
